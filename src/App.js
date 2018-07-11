e80f713ae0947ad7711791a901c8655335c4e93b
dargains <andre.dargains@gmail.com>
2018-07-11 17:39:17 +0100

    UPD: initial style update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       text: '',
       letters: 0,
       words: 0,
-      lorem: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid nam necessitatibus velit dolorum delectus quia doloremque accusamus quas praesentium facilis distinctio temporibus saepe explicabo cumque possimus neque asperiores animi exercitationem, illum ad excepturi, eligendi veniam molestiae! Explicabo iure deserunt vitae illo laborum facilis est, mollitia quia et illum, minima atque sequi commodi facere voluptate sint assumenda. Voluptate corporis sequi, facilis. Sunt iste libero rerum eum alias exam!'
+      lorem: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One month!'
     }
     this.onChange = this.onChange.bind(this);
     this.updateCounts = this.updateCounts.bind(this);
@@ -47,15 +47,24 @@ class App extends Component {
     const wordCount = this.state.words;
     return (
       <main>
-        <textarea ref="textarea" onChange={this.onChange} onDoubleClick={this.onClear} value={this.state.text} rows="6"/>
-        <br />
-        <button onClick={this.onCopy}>copy</button>
-        <button onClick={this.onClear}>clear</button>
+        <div className="leftContainer">
+          <h1 className="title">Letter Counter</h1>
+          <div className="about">
+            <h2 className="subtitle">Letter Counter</h2>
+            <p className="aboutText">Developed by Dargains, Foan82 & Vilaça</p>
+            <p className="aboutText">just to make your day better</p>
+          </div>
+        </div>
+        <div className="textarea">
+          <textarea ref="textarea" onChange={this.onChange} onDoubleClick={this.onClear} value={this.state.text} rows="6"/>
+          <button onClick={this.onClear}>clear</button>
+          <button onClick={this.onCopy}>copy</button>
+        </div>
         <h3>Letters: </h3>
         <p ref="letterCount">{letterCount}</p>
         <h3>Words: </h3>
         <p ref="wordCount">{wordCount}</p>
-        <h2>Lorem Ipsum Presets</h2>
+        <h2>Presets</h2>
         <button onClick={this.insertLorem.bind(this, 16)}>16 char</button>
         <button onClick={this.insertLorem.bind(this, 24)}>24 char</button>
         <button onClick={this.insertLorem.bind(this, 32)}>32 char</button>
